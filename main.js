cc362004207f157ac8e0f7f86d03a18ea4c75215
P0rtk3y <llama.p0rtk3y@gmail.com>
2020-01-21 21:09:31 -0800

    Done.

--- main.js
+++ main.js
@@ -4,9 +4,22 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeHearts = document.querySelectorAll('.like-glyph')
+likeHearts.forEach(likeHeart => likeHeart.addEventListener('click', () => heartClick(likeHeart)))
 
-
-
+const heartClick = (likeHeart) => {
+  mimicServerCall()
+    .then(() => {
+      likeHeart.innerHTML === FULL_HEART ? likeHeart.innerHTML = EMPTY_HEART : likeHeart.innerHTML = FULL_HEART 
+      likeHeart.className === 'activated-heart' ? likeHeart.classList.remove('activated-heart') : likeHeart.className = 'activated-heart'
+    })
+    .catch(error => {
+      const modal = document.querySelector('#modal')
+      modal.classList.remove('hidden')
+      modal.querySelector('p').innerText = error 
+      setTimeout(() => modal.className = 'hidden', 5000)
+    })
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
