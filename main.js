406d7a62b35ac8dc26d7b20aaa9a2156cc54fd45
scrabill <shannon.crabill@gmail.com>
2020-01-23 20:07:11 -0500

    Adds toggle and catch for server error

--- main.js
+++ main.js
@@ -5,28 +5,87 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 // But this only works for the first glyph
-// const likeGlyph = document.querySelector('.like-glyph');
+// const likeGlyph = document.querySelectorAll('.like-glyph');
+// const likeGlyph = document.getElementsByClassName("like-glyph")
+const likeGlyph = document.getElementsByClassName('like-glyph');
 
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("The DOM has loaded")
+  //
+  // console.log(likeGlyph)
 
-  document.addEventListener("click", (event) => {
-    console.log("A like has been clicked!")
-    // console.log(event.target.innerText)
+  addLikeListener();
 
-    if (event.target.innerHTML == EMPTY_HEART) {
-      console.log("SAD")
-      mimicServerCall();
-    } else if (event.target.innerHTML == FULL_HEART) {
-      console.log("Happy" )
-    }
 
-  })
+
+  // document.addEventListener("click", (event) => {
+  //   console.log("A like has been clicked!")
+  //   // console.log(event.target.innerText)
+  //
+  //   if (event.target.innerHTML == EMPTY_HEART) {
+  //     // console.log("SAD")
+  //     mimicServerCall();
+  //   } else if (event.target.innerHTML == FULL_HEART) {
+  //     // console.log("Happy" )
+  //   }
+  //
+  // })
 
 })
 
-function like() {
-  mimicServerCall();
+function addLikeListener() {
+  for (const e of likeGlyph) {
+    // console.log(e)
+    e.addEventListener("click", (e) => {
+      console.log("A like has been clicked!")
+      // console.log(event.target.innerText)
+
+      if (event.target.innerHTML == EMPTY_HEART) {
+        // console.log("SAD")
+        mimicServerCall()
+        .then((result) => {
+          updateHeart(e);
+        })
+        .catch((error) => {
+          console.log("Error!")
+          updateModel(e);
+        })
+      } else if (event.target.innerHTML == FULL_HEART) {
+        // console.log("Happy" )
+        updateModel(e);
+      }
+    })
+  }
+}
+
+function updateHeart(e) {
+  // console.log(e.target.innerHTML);
+  if (e.target.innerHTML == FULL_HEART) {
+    e.target.innerHTML = EMPTY_HEART;
+  } else if (e.target.innerHTML == EMPTY_HEART) {
+    e.target.innerHTML = FULL_HEART;
+  }
+
+  e.target.classList.add("activated-heart");
+}
+
+const model = document.querySelector('#modal')
+
+function updateModel() {
+
+
+  if (model.classList == "hidden") {
+    model.classList.remove("hidden");
+    setTimeout(hideModel, 5000)
+  } else {
+    model.classList.add("hidden");
+  }
+}
+
+function hideModel() {
+  console.log("...hiding model..")
+  model.classList.add("hidden");
+  console.log("...the model is now hidden..")
 }
 
 
