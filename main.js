7061d04a9dc0f1ab29f050b14e556e5d6661160f
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-01-30 18:40:15 -0800

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartBtns = document.getElementsByClassName('like-glyph')
+const modal = document.getElementById('modal')
+modal.className = "hidden"
 
+document.addEventListener("DOMContentLoaded", () => {
+  for (const like of heartBtns) {
+    like.addEventListener('click', function(e){
+      let heart = e.target
+      if (heart.innerText === EMPTY_HEART) {  
+        mimicServerCall()
+          .then(() => {
+            heart.className += ' activated-heart'
+            heart.innerText = `${FULL_HEART}`
+          })
+          .catch(error => {
+            modal.hidden = false
+            let errorMsg = document.getElementById("modal-message")
+            errorMsg.innerText = error
 
+            setTimeout(() => {
+              modal.className = 'hidden'
+            }, 5000)
+          })
+      } else {
+        heart.className -= ' activated-heart'
+        heart.innerText = `${EMPTY_HEART}`
+      }
+    })
+  }
+})
 
 
 //------------------------------------------------------------------------------
