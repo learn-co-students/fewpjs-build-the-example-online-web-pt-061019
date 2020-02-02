dc2066816d5239433f3cfc76e1c990ac9b123651
tshuttles <tshuttlesworth@gmail.com>
2020-02-01 23:26:45 -0600

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
+const hidden = document.querySelector("#modal")
+hidden.className = "hidden"
+const modalMsg = document.getElementById("modal-message")
 
+function heart() {
+  let hearts = document.getElementsByClassName("like-glyph")
+  for(heart of hearts){
+    heart.addEventListener('click', function(e){
+      mimicServerCall("url")
+      .then(function(){
+        if (e.target.innerText === EMPTY_HEART) {
+          e.target.className = "activated-heart"
+          e.target.innerText = FULL_HEART
+        }
+        else if (e.target.innerText === FULL_HEART) {
+          e.target.className = "like-glyph"
+          e.target.innerText = EMPTY_HEART 
+        }
+      })
+      .catch(function(error) {
+        setTimeout(function() {
+          hidden.className = "notHidden" 
+          modalMsg.innerText = error.message
+        }, 5000);
+      })
+    })
+  }
+};
 
+document.addEventListener("DOMContentLoaded", function() {
+  heart()
+});
 
 
 //------------------------------------------------------------------------------
@@ -22,4 +51,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       }
     }, 300);
   });
-}
+};
